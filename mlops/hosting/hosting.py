b443003387f5e526c39cc99d40bf3df5f07008be
jain-neeeraj <31276396+jain-neeeraj@users.noreply.github.com>
2026-09-24 21:50:04 +0530

    Improve readability by adding a blank line
    Added a blank line for better code readability.

--- mlops/hosting/hosting.py
+++ mlops/hosting/hosting.py
@@ -1,6 +1,7 @@
 from huggingface_hub import HfApi
 import os
 
+
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="mlops/deployment",     # the local folder containing your files
